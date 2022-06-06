9d1968eb666edfd804fb8af7b2e1ee2fa866cc39
kingBook <412126604@qq.com>
2022-06-06 17:44:58 +0800

    create

---
+++ Assets/Scenes/SyntaxTreeManualTraversal.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// C# 语法分析：https://docs.microsoft.com/zh-cn/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis
+
+public class SyntaxTreeManualTraversal : MonoBehaviour {
+
+    void Start() {
+        const string programText =
+            @"using System;
+            using System.Collections;
+            using System.Linq;
+            using System.Text;
+
+            namespace HelloWorld
+            {
+                class Program
+                {
+                    static void Main(string[] args)
+                    {
+                        Console.WriteLine(""Hello, World!"");
+                    }
+                }
+            }";
+
+        SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+        CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+        Debug.Log($"The tree is a {root.Kind()} node.");
+        Debug.Log($"The tree has {root.Members.Count} elements in it.");
+        Debug.Log($"The tree has {root.Usings.Count} using statements. They are:");
+        foreach (UsingDirectiveSyntax element in root.Usings) {
+            Debug.Log($"\t{element.Name}");
+        }
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+unity_testRoslyn
+
+C# 语法分析：
+https://docs.microsoft.com/zh-cn/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis
+
+Unity-Roslyn:
+https://github.com/mwahnish/Unity-Roslyn
