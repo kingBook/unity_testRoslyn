6907f4830d8ca951381b7c06b5caa13a8dc4cbfe
kingBook <412126604@qq.com>
2022-06-10 18:21:12 +0800

    update 2022-06-10 18:21

---
+++ Assets/Scenes/UsingCollector.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingCollector : CSharpSyntaxWalker {
+
+
+    private List<UsingDirectiveSyntax> m_usings = new List<UsingDirectiveSyntax>();
+
+    public ICollection<UsingDirectiveSyntax> usings => m_usings;
+
+    public override void VisitUsingDirective(UsingDirectiveSyntax node) {
+        Debug.Log($"\tVisitUsingDirective called with {node.Name}.");
+        if (node.Name.ToString() != "System" &&
+            !node.Name.ToString().StartsWith("System.")) {
+            Debug.Log($"\t\tSuccess. Adding {node.Name}.");
+            m_usings.Add(node);
+        }
+    }
+
+
+}
