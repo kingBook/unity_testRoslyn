c9cd284857315cbfb6927cbeb3fa61eea4a06339
kingBook <412126604@qq.com>
2022-06-23 23:50:31 +0800

    update 2022-06-23 23:50

---
+++ Assets/Scenes/添加替换命名空间/TestAddOrReplaceNameSpace.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAddOrReplaceNameSpace : MonoBehaviour {
+
+    private void Start() {
+        const string sampleCode =
+       @"using System;
+        using System.Collections;
+        using System.Linq;
+        using System.Text;
+
+        namespace HelloWorld
+        {
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    Console.WriteLine(""Hello, World!"");
+                }
+            }
+        }";
+
+        SyntaxTree tree = CSharpSyntaxTree.ParseText(sampleCode);
+        var root = (CompilationUnitSyntax)tree.GetRoot();
+
+
+        var childNodes = root.ChildNodes();
+        foreach (var childNode in childNodes) {
+            Debug.Log(childNode);
+        }
+
+        var identifierName = SyntaxFactory.IdentifierName("King").WithLeadingTrivia(SyntaxFactory.Whitespace(" ")).WithTrailingTrivia(SyntaxFactory.Whitespace(" "));
+        NamespaceDeclarationSyntax namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName("King"));
+        foreach (var m in root.Members) {
+            namespaceDeclaration = namespaceDeclaration.AddMembers(m);
+        }
+        Debug.Log(namespaceDeclaration);
+
+    }
+}
+
